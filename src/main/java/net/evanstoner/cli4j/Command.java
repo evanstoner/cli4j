4c3632893687cd097ef0d36bf22d1a166584aeee
Anthony Alves <aalves@fit.edu>
2017-05-24 12:37:08 -0400

    Change named and flag args to (long|short) opts
    Added the ability to have short or long opts to be treated as flags

--- src/main/java/net/evanstoner/cli4j/Command.java
+++ src/main/java/net/evanstoner/cli4j/Command.java
@@ -3,28 +3,38 @@ package net.evanstoner.cli4j;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
+import java.util.TreeMap;
 
 public abstract class Command {
 
-    public static enum ParameterOrder {
+
+    public enum ParameterOrder {
         NAMED_THEN_POSITIONAL,
         POSITIONAL_THEN_NAMED
     }
 
-    private String _baseCommand;
     private ParameterOrder _paramOrder = ParameterOrder.NAMED_THEN_POSITIONAL;
+
+    private String _baseCommand;
     private Command _parentCommmand = null;
     private String _parentCommandGlue = " ";
-    private String _namedPrefix = "--";
-    private String _flagPrefix = "-";
 
-    private HashMap<String, String> _named = new HashMap<String, String>();
-    private HashMap<Integer, String> _positional = new HashMap<Integer, String>();
-    private ArrayList<String> _flags = new ArrayList<String>();
+    private String _longOptsGlue = " ";
+    private String _longOptsPrefix = "--";
+    private String _shortOptsPrefix = "-";
+
+    private HashMap<String, String> _longOpts = new HashMap<>();
+
+    /**
+     * Short opts support options with values or treated as pure flags
+     * <p>
+     * flags will have a value of `null`
+     */
+    private HashMap<String, String> _shortOpts = new HashMap<>();
+
+    // Sorted map
+    private TreeMap<Integer, String> _positional = new TreeMap<>();
 
 
     /**
@@ -61,28 +71,51 @@ public abstract class Command {
         return this;
     }
 
+
     /**
-     * Set the prefix to place before named arguments. Defaults to two dashes ("--").
+     * Set the glue used to combine long option with their value
+     * Default: ' ' (space)
      *
-     * @param prefix The prefix string.
+     * @param glue The glue string.
      * @return this
      */
-    protected Command namedPrefix(String prefix) {
-        _namedPrefix = prefix;
+    public Command longOptsGlue(String glue) {
+        _longOptsGlue = glue;
         return this;
     }
 
     /**
-     * Set the prefix to place before flag arguments. Defaults to a dash ("-").
+     * Set the prefix to place before long options.
+     * <p>
+     * Defaults to two dashes ("--").
      *
      * @param prefix The prefix string.
      * @return this
      */
-    protected Command flagPrefix(String prefix) {
-        _flagPrefix = prefix;
+    protected Command longOptsPrefix(String prefix) {
+        _longOptsPrefix = prefix;
         return this;
     }
 
+    /**
+     * Set the prefix to place before short options.
+     * <p>
+     * Defaults to a dash ("-").
+     *
+     * @param prefix The prefix string.
+     * @return this
+     */
+    protected Command shortOptsPrefix(String prefix) {
+        _shortOptsPrefix = prefix;
+        return this;
+    }
+
+    /**
+     * Set the order of options and arguments when building the command
+     *
+     * @param order ParameterOrder enum
+     * @return this
+     */
     protected Command parameterOrder(ParameterOrder order) {
         _paramOrder = order;
         return this;
@@ -90,30 +123,60 @@ public abstract class Command {
 
 
     /**
-     * Set a named argument.
+     * Set a long option.
      *
-     * @param name  The name of the argument as it should appear in the command, but without the prefix.
+     * @param opt   The name of the option as it should appear in the command, but without the prefix.
      * @param value The value of the argument.
      */
-    protected void named(String name, String value) {
-        _named.put(name, value);
+    protected void longOption(String opt, String value) {
+        _longOpts.put(opt, value);
     }
 
     /**
-     * Set a positional argument.
+     * Set a long option as a flag
+     *
+     * @param name The name of the argument as it should appear in the command, but without the prefix.
+     */
+    protected void longOption(String name) {
+        longOption(name, null);
+    }
+
+    /**
+     * Set a short option.
+     *
+     * @param opt   The name of the option as it should appear in the command, but without the prefix.
+     * @param value The value of the argument.
+     */
+    protected void shortOption(String opt, String value) {
+        _shortOpts.put(opt, value);
+    }
+
+    /**
+     * Set a short option as a flag
+     *
+     * @param name The name of the argument as it should appear in the command, but without the prefix.
+     */
+    protected void shortOption(String name) {
+        shortOption(name, null);
+    }
+
+    /**
+     * Set a short option as a flag
+     *
+     * @param name The name of the argument as it should appear in the command, but without the prefix.
+     */
+    protected void shortOption(char name) {
+        shortOption(String.valueOf(name), null);
+    }
+
+    /**
+     * Set an argument.
      *
      * @param index The index of the argument.
      * @param value The value of the argument.
      */
     protected void positional(int index, String value) {
         _positional.put(index, value);
-    }
-
-    /**
-     * @param name
-     */
-    protected void flag(String name) {
-        _flags.add(name);
     }
 
 
@@ -123,6 +186,8 @@ public abstract class Command {
         String command;
 
         command = build();
+
+        // TODO use ProcessBuilder
 
         // execute the command, capturing the exit code and output
         p = Runtime.getRuntime().exec(command);
@@ -158,29 +223,47 @@ public abstract class Command {
             base = _parentCommmand.build() + _parentCommandGlue;
         }
 
-        StringBuilder namedSb = new StringBuilder();
-        StringBuilder positionalSb = new StringBuilder();
-        List<Integer> positionalIndices;
+        StringBuilder flagsSb = new StringBuilder(_shortOptsPrefix);
+        StringBuilder shortSb = new StringBuilder();
+        StringBuilder longSb = new StringBuilder();
 
-        for (String name : _named.keySet()) {
-            //TODO: missing named arguments
-            namedSb.append(_namedPrefix + name + " " + _named.get(name) + " ");
+        StringBuilder positionalSb = new StringBuilder();
+
+        _shortOpts.forEach((key, value) -> {
+            if (value == null) {
+                flagsSb.append(key);
+            } else {
+                shortSb.append(_shortOptsPrefix).append(key).append(" ");
+                if (!value.trim().isEmpty()) {
+                    shortSb.append(value).append(" ");
+                }
+            }
+        });
+
+        // if there are no flags, remove the flags prefix
+        if (flagsSb.length() <= _shortOptsPrefix.length()) {
+            flagsSb.delete(0, flagsSb.length());
         }
 
-        positionalIndices = new ArrayList<>(_positional.keySet());
-        Collections.sort(positionalIndices);
+        _longOpts.forEach((key, value) -> {
+            if (value == null) {
+                longSb.append(_longOptsPrefix).append(key).append(" ");
+            } else {
+                longSb.append(_longOptsPrefix).append(key).append(_longOptsGlue).append(value).append(" ");
+            }
+        });
 
-        for (Integer i : positionalIndices) {
-            //TODO: missing positional arguments
-            positionalSb.append(_positional.get(i) + " ");
+        // _positional is a TreeMap, which has sorted Keys in natural ordering
+        for (String arg : _positional.values()) {
+            positionalSb.append(arg).append(" ");
         }
 
         base += _baseCommand;
 
         if (_paramOrder == ParameterOrder.NAMED_THEN_POSITIONAL) {
-            base += emptyOrString(namedSb) + emptyOrString(positionalSb);
+            base += emptyOrString(flagsSb) + emptyOrString(shortSb) + emptyOrString(longSb) + emptyOrString(positionalSb);
         } else if (_paramOrder == ParameterOrder.POSITIONAL_THEN_NAMED) {
-            base += emptyOrString(positionalSb) + emptyOrString(namedSb);
+            base += emptyOrString(positionalSb) + emptyOrString(flagsSb) + emptyOrString(shortSb) + emptyOrString(longSb);
         }
 
         return base;
@@ -189,6 +272,8 @@ public abstract class Command {
     /**
      * Get an empty string or string with prepended space char
      * when building commands
+     * <p>
+     * Useful when building commands and only want 1 or 0 spaces between an argument
      *
      * @param sb string builder to check if empty
      * @return empty string if string build is empty,
--- src/test/java/net/evanstoner/cli4j/CommandTest.java
+++ src/test/java/net/evanstoner/cli4j/CommandTest.java
@@ -44,25 +44,6 @@ public class CommandTest {
     }
 
     @Test
-    public void testGolangProgramParams() {
-        Command go = new Command("go") {
-        };
-
-        Command run = new Command("run", go) {
-        };
-
-        Command golangProg = new Command("myprog.go", run) {
-        };
-
-        // temporarily use namedprefix until new changes
-        golangProg.namedPrefix("-");
-        golangProg.named("raddr", "localhost:8080");
-        golangProg.named("delay", "200ms");
-
-        assertEquals("go run myprog.go -delay 200ms -raddr localhost:8080", golangProg.build());
-    }
-
-    @Test
     public void successfulCommandOutputStreamIsSet() throws IOException, InterruptedException {
         String cmd = (OS.contains("win")) ? "dir" : "ls";
 
@@ -90,7 +71,7 @@ public class CommandTest {
 
         curl.parameterOrder(Command.ParameterOrder.POSITIONAL_THEN_NAMED);
         curl.positional(0, "https://www.google.com");
-        curl.named("http2", "");
+        curl.longOption("http2");
 
         assertEquals("curl https://www.google.com --http2", curl.build());
     }
@@ -111,5 +92,246 @@ public class CommandTest {
     public void prependedStringOnNonEmptyStringBuilder() {
         assertEquals(" my data", new Command("") {
         }.emptyOrString(new StringBuilder("my data")));
+    }
+
+    @Test
+    public void testTreeMapPositionals() {
+        int iterations = 100;
+
+        Command echo = new Command("echo") {
+        };
+
+        StringBuilder sb = new StringBuilder("echo ");
+        for (int i = 0; i < iterations; i++) {
+            echo.positional(i, String.valueOf(i));
+            sb.append(i).append(" ");
+        }
+
+        assertEquals(sb.toString().trim(), echo.build());
+    }
+
+    @Test
+    public void testGolangProgramParams() {
+        Command go = new Command("go") {
+        };
+
+        Command run = new Command("run", go) {
+        };
+
+        Command golangProg = new Command("myprog.go", run) {
+        };
+
+        golangProg.shortOption("raddr", "localhost:8080");
+        golangProg.shortOption("delay", "200ms");
+
+        assertEquals("go run myprog.go -delay 200ms -raddr localhost:8080", golangProg.build());
+    }
+
+    @Test
+    public void testRsyncFlagsWithPaths() {
+        Command rsync = new Command("rsync") {
+        };
+
+        rsync.shortOption("a");
+        rsync.shortOption("z");
+        rsync.positional(0, "user@localhost:/var/log/apache2");
+        rsync.positional(1, "/home/user/Desktop/");
+        assertEquals("rsync -az user@localhost:/var/log/apache2 /home/user/Desktop/", rsync.build());
+    }
+
+    @Test
+    public void testCommandWithSpacesInDoubleQuotes() {
+        Command echo = new Command("echo") {
+        };
+
+        echo.positional(0, "\"this is my echo test\"");
+
+        assertEquals("echo \"this is my echo test\"", echo.build());
+    }
+
+    @Test
+    public void testCommandWithSpacesInSingleQuotes() {
+        Command echo = new Command("echo") {
+        };
+
+        echo.positional(0, "\'this is my echo test\'");
+        assertEquals("echo \'this is my echo test\'", echo.build());
+    }
+
+    @Test
+    public void testCommandWithPiping() {
+        Command ls = new Command("ls") {
+        };
+
+        ls.shortOption("l");
+        ls.shortOption("ah");
+        ls.positional(0, "~");
+
+        Command pipe = new Command("|", ls) {
+        };
+
+        Command grep = new Command("grep", pipe) {
+        };
+
+        grep.shortOption("nri");
+        grep.positional(0, "\'Desktop\'");
+
+        assertEquals("ls -ahl ~ | grep -nri \'Desktop\'", grep.build());
+    }
+
+    @Test
+    public void testIPtablesBlockIP() {
+        Command iptables = new Command("iptables") {
+        };
+
+        iptables.shortOption("A", "INPUT");
+        iptables.shortOption("s", "\"10.23.32.21\"");
+        iptables.shortOption("j", "DROP");
+        assertEquals("iptables -A INPUT -s \"10.23.32.21\" -j DROP", iptables.build());
+    }
+
+    @Test
+    public void parentCommandGlueWithGitFlow() {
+        Command git = new Command("git") {
+        };
+
+        Command gitFlow = new Command("flow", git) {
+        }.parentCommandGlue("-");
+
+        Command gitFlowInit = new Command("init", gitFlow) {
+        };
+
+
+        gitFlowInit.shortOption('d');
+
+        assertEquals("git-flow init -d", gitFlowInit.build());
+    }
+
+
+    class AddFixedIpCommand extends Command {
+
+        AddFixedIpCommand(NovaCli nova) {
+            super("add-fixed-ip", nova);
+        }
+
+        AddFixedIpCommand server(String serverNameOrId) {
+            positional(0, serverNameOrId);
+            return this;
+        }
+
+        AddFixedIpCommand networkId(String networkId) {
+            positional(1, networkId);
+            return this;
+        }
+    }
+
+    class NovaCli extends Command {
+
+        NovaCli(String username, String password, String tenantName, String authUrl) {
+            super("nova");
+            longOption("os-username", username);
+            longOption("os-password", password);
+            longOption("os-tenant-name", tenantName);
+            longOption("os-auth-url", authUrl);
+        }
+
+        AddFixedIpCommand addFixedIp() {
+            return new AddFixedIpCommand(this);
+        }
+    }
+
+    @Test
+    public void testNovaLongOption() {
+        NovaCli nova = new NovaCli("user", "passw0rd", "tenant", "http://openstack-server:5000");
+        String cmd = nova.addFixedIp().server("my-server").networkId("some-uuid").build();
+
+        assertEquals("nova --os-tenant-name tenant --os-username user --os-auth-url " +
+                "http://openstack-server:5000 --os-password passw0rd add-fixed-ip my-server some-uuid", cmd);
+    }
+
+    @Test
+    public void testJavaMemoryConstraints() {
+        Command java = new Command("java") {
+        };
+
+        java.shortOption("Xms32M", "");
+        java.shortOption("Xmx1024M", "");
+        java.positional(0, "MyProgram");
+
+        assertEquals("java -Xmx1024M -Xms32M MyProgram", java.build());
+    }
+
+    @Test
+    public void testJavaMemoryConstraintsPositionalsFirst() {
+        Command java = new Command("java") {
+        };
+        java.parameterOrder(Command.ParameterOrder.POSITIONAL_THEN_NAMED);
+
+        java.shortOptsPrefix("-X");
+        java.shortOption("ms32M", "");
+        java.shortOption("mx1024M", "");
+        java.positional(0, "MyProgram");
+
+        assertEquals("java MyProgram -Xmx1024M -Xms32M", java.build());
+    }
+
+    @Test
+    public void testCommandWithCustomShortOptsPrefix() {
+        Command docker = new Command("docker") {
+        };
+
+        Command dockerRun = new Command("run", docker) {
+        };
+
+        dockerRun.shortOptsPrefix("#");
+        dockerRun.shortOption("it");
+        dockerRun.positional(0, "ubuntu");
+        dockerRun.positional(1, "/bin/bash");
+
+
+        assertEquals("docker run #it ubuntu /bin/bash", dockerRun.build());
+    }
+
+    @Test
+    public void complexCurlHTTPRequest() {
+
+        Command curl = new Command("curl") {
+        };
+
+        curl.shortOption("v");
+        curl.shortOption('0');
+        curl.longOption("compressed");
+        curl.shortOption("c", "\"cookiejar.txt\"");
+        curl.longOption("cookie", "\"authretries=0\"");
+        curl.positional(0, "http://example.com");
+
+
+        // verbose
+        // use HTTP/1.0
+        // enable compression
+        // write all cookies to 'cookiejar.txt'
+        // set request cookie to 'authretries=0'
+        assertEquals("curl -0v -c \"cookiejar.txt\" --cookie \"authretries=0\" --compressed http://example.com", curl.build());
+    }
+
+    @Test
+    public void testCommandWithCustomShortOptsAndCustomLongOptPrefixWithLongGlue() {
+        Command docker = new Command("docker") {
+        };
+
+        Command dockerRun = new Command("run", docker) {
+        };
+
+        dockerRun.shortOptsPrefix("#");
+        dockerRun.longOptsPrefix("--D");
+        dockerRun.shortOption("it");
+
+        dockerRun.longOptsGlue("=");
+        dockerRun.longOption("network", "my_docker_network");
+
+        dockerRun.positional(0, "ubuntu");
+        dockerRun.positional(1, "/bin/bash");
+
+        assertEquals("docker run #it --Dnetwork=my_docker_network ubuntu /bin/bash", dockerRun.build());
     }
 }
